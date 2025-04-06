a9b429d8d47bfce38b80b196cb8097da8c56ae77
Ahmed Aslam <ahmedaslam2004@gmail.com>
2025-04-05 21:17:29 -0400

    current eboard and jboard added

--- ieee-ua-website/src/pages/Board.js
+++ ieee-ua-website/src/pages/Board.js
@@ -45,6 +45,7 @@ function Board() {
       <div className ='button-lay'>
       <select id='year' onChange={(e) => setYear(e.target.value)} value ={year}>
           <option disabled selected>Year</option>
+          <option value="2025">2025</option>
           <option value="2024">2024</option>
           <option value="2023">2023</option>
           <option value="2022">2022</option>
